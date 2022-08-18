689e09bf0102581d8f74b9da624a86c9d5b8eddb
Duterfresh <582544712@qq.com>
2022-08-18 18:21:11 +0800

    del(文件）：删除一些不必要文件

--- cmake-build-release/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/CLionProjects/multiple_reactor
-# Build directory: D:/CLionProjects/multiple_reactor/cmake-build-release
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("multiple_reactor")
